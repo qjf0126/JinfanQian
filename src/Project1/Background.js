654ff03c8afd4cc839d8445cf88269bfd6565c9b
qjf0126 <154873378+qjf0126@users.noreply.github.com>
2024-04-23 13:29:59 +0800

    Add files via upload

--- src/Project1/Background.js
+++ src/Project1/Background.js
@@ -25,9 +25,9 @@ const Background = () => {
         {/* 右侧区块：项目背景介绍 */}
         <section className="Project1-background">
           <h2>BACKGROUND</h2>
-          <p style={{ fontSize: '14px', color: '#454545' }}>In August 2023, I visited Japan for tourism. After a week of sightseeing, our trip ended with a return flight from KIX. Despite checking Google Maps and social media apps for traffic updates, we almost got lost trying to find the right train platform to the airport. When we finally reached the airport, the check-in counter was closed.</p>
-          <p style={{ fontSize: '14px', color: '#454545' }}>Later, I found out that many of my friends had similar experiences when flying from foreign airports. They feel anxious and worried, facing unexpected situations that can cause them to miss their flights.</p>
-          <p style={{ fontSize: '16px', color: '#121212', fontStyle: 'italic' }}>So, how might we help people who travel abroad to catch flights easily and confidently?</p>
+          <p style={{ fontSize: '14px', color: '#707070' }}>In August 2023, I visited Japan for tourism with my friends. After a week of sightseeing, our trip ended with a return flight from KIX. Despite checking Google Maps and social media apps for traffic updates, we almost got lost trying to find the right train platform to the airport. When we finally reached the airport, the check-in counter was closed.</p>
+          <p style={{ fontSize: '14px', color: '#707070' }}>Later, I found out that many of my friends had similar experiences when flying from foreign airports. They feel anxious and worried, facing unexpected situations that can cause them to miss their flights.</p>
+          <p className='HMW' style={{ fontSize: '16px', color: '#454545'}}>💡So, how might we help people who travel abroad to catch flights easily and confidently?</p>
           {/* 添加更多内容... */}
         </section>
       </div>
