ecff518cf24d6e0118a1a1d4634efe59c758abd3
qjf0126 <154873378+qjf0126@users.noreply.github.com>
2024-10-25 14:41:51 +0800

    Add files via upload

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ export const Banner = () => {
       <Container className="bannercontainer">
         <div className="bannertext">
           <h1>Hi, I'm Jinfan Qian</h1>
-          <p style={{fontSize:'18px'}}>I'm an Art and Technology undergraduate at Zhejiang University, passionate about UX design and communication design.</p>
+          <p style={{fontSize:'18px'}}>An Art and Tech undergraduate at <a href="https://www.zju.edu.cn/english/" target="_blank" className="zju-link">ZJU</a>, passionate about UX, communication design and physical prototyping.</p>
           <a href="/about"><button>more about me</button></a>
         </div>
         <div>
