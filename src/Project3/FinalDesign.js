bf039185b21fb8c9caf93c40cee1bb5f9cf222f3
qjf0126 <154873378+qjf0126@users.noreply.github.com>
2024-10-29 02:41:24 +0800

    Add files via upload

--- src/Project3/FinalDesign.js
+++ src/Project3/FinalDesign.js
@@ -8,6 +8,7 @@ const FinalDesign = () => {
         <p className="final3-paragraph">
           Our community is being flooded with food delivery waste. Navigate your life and make choices that shape your community's future. <a href="https://qjf030126.itch.io/tide-of-takeout-waste" target="_blank" rel="noopener noreferrer">Play the Game <i className="bi bi-box-arrow-up-right"></i></a>
         </p>
+        <iframe width="860" height="547" src="https://www.youtube.com/embed/euCEkwGyx8k?si=o2l9Jg_XSO_fxBVq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
       </div>
     </section>
   );
