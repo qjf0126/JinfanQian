0ef2a4ddd760ec796e2e4e19dd1d29d62afe3317
qjf0126 <154873378+qjf0126@users.noreply.github.com>
2024-12-15 16:39:48 +0800

    Add files via upload

--- src/Project2/Footer.js
+++ src/Project2/Footer.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
+    const scrollToTop = () => {
+        const target = document.getElementById('overview2');
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
-      <section id="footer2">
-        <div className="footer2-container">
-          <h2 className="footer2-header">
-            <NavLink to="https://museaward.com/winner-info.php?id=229805" className='winnerpage'>
-              <p style={{margin:'4px 4px 4px 0px'}}>More Project</p>
-              <p style={{margin:'4px 0px 4px 4px'}}>→</p>
-            </NavLink>
-          </h2>
-        </div>
-      </section>
+        <section id="footer2">
+            <div className="footer2-container">
+                <h2 className="footer2-header">
+                  <NavLink to="https://museaward.com/winner-info.php?id=229805" className="winnerpage">
+                    <p style={{ margin: '4px 4px 4px 0px' }}>More Project</p>
+                    <p style={{ margin: '4px 0px 4px 4px' }}>→</p>
+                  </NavLink>
+                  <button className='nextProj' onClick={scrollToTop} >
+                    Back to Top
+                  </button>
+                </h2>
+            </div>
+        </section>
     );
-  };
-  
-  export default Footer;
+};
+
+export default Footer;
