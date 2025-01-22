7b10334e75eaff62a84a3d7b44f76bea10fb67a7
qjf0126 <154873378+qjf0126@users.noreply.github.com>
2025-01-23 02:01:11 +0800

    Add files via upload

--- src/Project4/Display.js
+++ src/Project4/Display.js
@@ -1,6 +1,7 @@
 // components/Display.js
 import React from 'react';
 import final1 from './img/final1.mp4';
+import final2 from './img/final2.mp4';
 
 const Display = () => {
   return (
@@ -10,14 +11,24 @@ const Display = () => {
         </div>
         <div className="display4-container">
           <h2 className='display4-h2'>EFFECT DISPLAY</h2>
-          <video 
-            src={final1}
-            controls 
-            className="display4-video"
-            style={{width: '50%', marginTop: '20px'}}
-          >
-            Your browser does not support the video tag.
-          </video>
+          <div style={{display:'flex', justifyContent:'space-between'}}>
+            <video 
+              src={final1}
+              controls 
+              className="display4-video"
+              style={{width: '49%', marginTop: '20px'}}
+            >
+              Your browser does not support the video tag.
+            </video>
+            <video 
+              src={final2}
+              controls 
+              className="display4-video"
+              style={{width: '49%', marginTop: '20px'}}
+            >
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
     </section>
   );
